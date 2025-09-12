e48eeb0c96588e65e6289cbbcac3a2799ec1f4db
AndreiVed <catblues87@gmail.com>
2025-09-12 19:20:27 +0300

    added errors handler to main script

--- main.py
+++ main.py
@@ -5,31 +5,46 @@ API_URL = "https://jsonplaceholder.typicode.com/users"
 HEADERS = ("id", "name", "email")
 
 
-def get_response() -> list:
-    response = requests.get(API_URL)
-    print(f"Status Code: {response.status_code}")
+def get_response() -> list | None:
+    try:
+        response = requests.get(API_URL)
+        print(f"Status Code: {response.status_code}")
+        response.raise_for_status()
 
-    users_data = response.json()
-    data = [
-        {"id": user["id"], "name": user["name"], "email": user["email"]}
-        for user in users_data
-    ]
+        users_data = response.json()
+        if not isinstance(users_data, list):
+            raise ValueError("Data is not a list")
 
-    return data
+        return [
+            {"id": user["id"], "name": user["name"], "email": user["email"]}
+            for user in users_data
+        ]
+
+    except Exception as e:
+        print(f"An unexpected error occurred: {e} ")
+
+    return None
 
 
 def convert_to_csv(data: list) -> None:
-    with open("result_data.csv", "w", newline="") as csvfile:
-        writer = csv.DictWriter(csvfile, fieldnames=HEADERS)
-        writer.writeheader()
-        writer.writerows(data)
-
-    print("Users data just written to 'result_data.csv'")
+    try:
+        with open("result_data.csv", "w", newline="") as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=HEADERS)
+            writer.writeheader()
+            writer.writerows(data)
+        print("Users data just written to 'result_data.csv'")
+    except IOError as e:
+        print(f"An unexpected error while writing data to file: {e} ")
+        raise
 
 
 def main() -> None:
-    data = get_response()
-    convert_to_csv(data)
+    try:
+        data = get_response()
+        if data:
+            convert_to_csv(data)
+    except Exception as e:
+        print(f"Operation ended with error: {e}")
 
 
 if __name__ == "__main__":
